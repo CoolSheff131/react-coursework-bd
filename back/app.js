ff116e94e2fd3e4d91406a5ed62fd1242a6d99c1
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-10-25 22:29:46 +0800

    init

---
+++ back/app.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test',
+});
+// тестирование подключения
+connection.connect(function (err) {
+  if (err) {
+    return console.error('Ошибка: ' + err.message);
+  } else {
+    console.log('Подключение к серверу MySQL успешно установлено');
+  }
+});
+// закрытие подключения
+connection.end(function (err) {
+  if (err) {
+    return console.log('Ошибка: ' + err.message);
+  }
+  console.log('Подключение закрыто');
+});
