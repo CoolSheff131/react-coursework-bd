917f5f6f467e59547d57549a04d725d579798cbe
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-11-16 14:11:33 +0800

    Подключен Express

--- back/app.js
+++ back/app.js
@@ -1,35 +1,41 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/', (request, response) => {
+  response.send('Hello from Express!');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
 });
-
-const mysql = require('mysql2');
+///////////////////////////////
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'test',
 });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected!');
+  connection.query('SELECT * FROM test', function (err, result) {
+    if (err) throw err;
+    console.log('Result: ' + result);
+  });
+});
 // тестирование подключения
-connection.execute('SELECT * FROM test', function (err, results, fields) {
-  console.log(err);
-  console.log(results); // собственно данные
-  console.log(fields); // мета-данные полей
-});
+// connection.execute('SELECT * FROM test', function (err, results, fields) {
+//   console.log(err);
+//   console.log(results); // собственно данные
+//   console.log(fields); // мета-данные полей
+//});
 // закрытие подключения
-connection.end(function (err) {
-  if (err) {
-    return console.log('Ошибка: ' + err.message);
-  }
-  console.log('Подключение закрыто');
-});
+// connection.end(function (err) {
+//   if (err) {
+//     return console.log('Ошибка: ' + err.message);
+//   }
+//   console.log('Подключение закрыто');
+// });
