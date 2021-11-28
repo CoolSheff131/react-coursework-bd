a647cc78b462a91026035048a4c02227bfd20db4
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-11-28 21:31:03 +0800

    Добавлены заголовки на карточке документа

--- front/src/components/CardDocument.tsx
+++ front/src/components/CardDocument.tsx
@@ -7,21 +7,27 @@ interface DocumentProp {
 function CardDocument(prop: DocumentProp) {
     const { document } = prop;
     return (
-        <Card bg="dark" text="white">
+        <Card border="dark">
             <Card.Header as="h5">{document.name}</Card.Header>
             <Card.Body>
+                <h6>Название организации</h6>
                 <Card.Title>{document.organizationName}</Card.Title>
                 <Card.Text>
+                    <h6>ФИО автора</h6>
                     {document.firstNameAuthor} {document.secondNameAuthor}
+                    <h6>Номер</h6>
                     {document.number}
+                    <h6>Количество страниц</h6>
                     {document.pageCount}
+                    <h6>Тип документа</h6>
                     {document.type}
+                    <h6>Год</h6>
                     {document.year}
                 </Card.Text>
                 <ButtonGroup size="sm" >
-                    <Button variant="light">Delete</Button>
-                    <Button variant="light">Change</Button>
-                    <Button variant="light">Show</Button>
+                    <Button variant="dark">Delete</Button>
+                    <Button variant="dark">Change</Button>
+                    <Button variant="dark">Show</Button>
                 </ButtonGroup>
             </Card.Body>
         </Card>
